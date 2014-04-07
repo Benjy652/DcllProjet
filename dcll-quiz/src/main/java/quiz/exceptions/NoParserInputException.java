b1771c40aa989d9e55b6e725ccc45a19264dcd8f
Assyl Louahadj <assyl.louahadj@gmail.com>
2014-04-07 14:38:45 +0200

    #12 Création des classes pour les excéptions du Parser

---
+++ dcll-quiz/src/main/java/quiz/exceptions/NoParserInputException.java
@@ -0,0 +1,12 @@
+package quiz.exceptions;
+
+/**
+ * 
+ * @author Assyl
+ *
+ */
+public class NoParserInputException extends Exception{
+	public NoParserInputException() {
+		super("Error : No input to parse!");
+	}
+}
---
+++ dcll-quiz/src/main/java/quiz/exceptions/WrongSyntaxException.java
@@ -0,0 +1,15 @@
+package quiz.exceptions;
+
+/**
+ * 
+ * @author Assyl
+ *
+ */
+public class WrongSyntaxException extends Exception {
+	
+	
+	WrongSyntaxException(String errorMessage){
+		super(errorMessage);
+	}
+
+}
